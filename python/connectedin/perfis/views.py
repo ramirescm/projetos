6cd6280de6cf7e987110c917ea0f9437c2626145
ramires <ramirescm@gmail.com>
2017-05-31 22:39:18 -0300

    criação de rotas e views e controllers

--- python/connectedin/perfis/views.py
+++ python/connectedin/perfis/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from perfis.models import Perfil
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html', { 'perfis' : Perfil.objects.all()})
+
+def exibir(request, perfil_id):
+    perfil = Perfil.objects.get(id=perfil_id)
+    return render(request, 'perfil.html', { "perfil" : perfil })
+
+def convidar(request, perfil_id):
+    pass
+
+def get_perfil_logado(request):
+   return Perfil.objects.get(id=1) 
+
