f4e670a89be34ed0add73766c2e09ab1587362bd
ramires <ramirescm@gmail.com>
2017-06-10 23:49:22 -0300

    Projeto estudo AspNetCore

---
+++ c#/CasaDoCodigo/CasaDoCodigo/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Carrosel()
+        {
+            return View();
+        }
+
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+
+        public IActionResult Resumo()
+        {
+            return View();
+        }
+    }
+}
