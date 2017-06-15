7e385909781a11b9e5ee992e42153cbc1445e5f0
ramires <ramirescm@gmail.com>
2017-06-14 23:02:34 -0300

    Finalizado primeira parte do projeto com EF, LINQ, Persistência

--- c#/CasaDoCodigo/CasaDoCodigo/Controllers/PedidoController.cs
+++ c#/CasaDoCodigo/CasaDoCodigo/Controllers/PedidoController.cs
@@ -3,24 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CasaDoCodigo.Models;
+using CasaDoCodigo.Models.ViewModel;
 
 namespace CasaDoCodigo.Controllers
 {
     public class PedidoController : Controller
     {
+        private readonly IDataService _dataService;
+
+        public PedidoController(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
         public IActionResult Carrosel()
         {
-            return View();
+            var produtos = _dataService.GetProdutos();
+            return View(produtos);
         }
 
         public IActionResult Carrinho()
         {
-            return View();
+            CarrinhoViewModel carrinhoViewModel = GetCarrinhoViewModel();
+            return View(carrinhoViewModel);
+        }
+
+        private CarrinhoViewModel GetCarrinhoViewModel()
+        {
+            var produtos = _dataService.GetProdutos();
+            var itensCarrinho = _dataService.GetItensPedido();
+
+            var carrinhoViewModel = new CarrinhoViewModel(itensCarrinho);
+            return carrinhoViewModel;
         }
 
         public IActionResult Resumo()
         {
-            return View();
+            CarrinhoViewModel carrinhoViewModel = GetCarrinhoViewModel();
+            return View(carrinhoViewModel);
         }
     }
 }
---
+++ c#/CasaDoCodigo/CasaDoCodigo/IDataService.cs
@@ -0,0 +1,14 @@
+﻿using CasaDoCodigo.Models;
+using System.Collections.Generic;
+
+namespace CasaDoCodigo
+{
+    public interface IDataService
+    {
+        void InicialiaDB();
+
+        List<Produto> GetProdutos();
+
+        List<ItemPedido> GetItensPedido();
+    }
+}
